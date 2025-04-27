f904559ac53232d562c1fe0eee9bcf8c6635f5b6
Alex Oliveira <alexoliveira7x@gmail.com>
2025-04-27 20:11:06 +0100

    feat: create template

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 import { parseArgs } from "node:util";
-import { ERR_MISSING_TEMPLATE } from "./errors.js";
+import { ERR_MISSING_TEMPLATE, ERR_FETCH_TEMPLATE } from "./errors.js";
+import { resolve, extname, dirname } from "node:path";
+import { mkdirSync } from "node:fs";
+import { writeFile } from "fs/promises";
+import { Writable } from "node:stream";
+
+// ===========
+// Initialization
+// ===========
 
 const options = {
+  verbose: {
+    type: "boolean",
+    short: "v",
+    default: false,
+  },
   extension: {
     type: "string",
     short: "e",
@@ -13,18 +26,112 @@ const options = {
     type: "string",
     short: "t",
   },
+  user: {
+    type: "string",
+    short: "u",
+  },
+  output: {
+    type: "string",
+    short: "o",
+  },
 };
+
+// ===========
+// Helpers
+// ===========
+
+const logStream = new Writable({
+  write(chunk, encoding, callback) {
+    process.stdout.write(chunk, encoding, callback);
+  },
+});
+
+function log(...args) {
+  if (values.verbose) {
+    logStream.write(`YNDAP: ${args.join(" ")}\n`);
+  }
+}
+
+/* @function buildTemplate
+ * @description Build the template URL based on the provided options.
+ * Official ydnap templates are hosted on GitHub
+ * https://raw.githubusercontent.com/:user/:repo/:branch/:type/index.js
+ * 3th party templates
+ * https://raw.githubusercontent.com/:user/:repo/:branch/:path
+ */
+function buildTemplate() {
+  const branch = "main";
+  const repo = "guildadev/ydnap-templates";
+  const type = values.extension;
+  const template = values.template;
+  const path = `src/${type}/${template}/index.${type}`;
+
+  if (values.user) {
+    path = `${type}/${values.user}/src/${template}/index.${type}`;
+    log(`Building template URL: ${path}`);
+  }
+
+  const url = `https://raw.githubusercontent.com/${repo}/${branch}/${path}`;
+  log(`Template URL: ${url}`);
+  return {
+    url,
+  };
+}
+
+async function resolveOutput() {
+  const { output, template, extension } = values;
+
+  if (output) {
+    const outputPath = resolve(process.cwd(), output);
+    const outputExt = extname(output);
+    log("Output", outputPath);
+
+    if (outputExt) {
+      // When output is a file
+      // Ensure its parent directory exists
+      const parentDir = dirname(outputPath);
+      mkdirSync(parentDir, { recursive: true });
+      log("Output is a file, parentDir:", parentDir);
+      return outputPath;
+    } else {
+      // When output is a directory
+      mkdirSync(outputPath, { recursive: true });
+      return resolve(outputPath, `${template}.${extension}`);
+    }
+  }
+
+  // No output provided → use template name in cwd
+  return resolve(process.cwd(), `${template}.${extension}`);
+}
 
 const { values } = parseArgs({ options, tokens: true });
 
+// ===========
+// Validation
+// ===========
+
 if (!values.template) {
-  console.error(
+  log(
     "Usage: ydnap -t <js|ts> <template> [-u githubuser | --user githubuser | -r user/repo | --repo user/repo]",
   );
   throw new TypeError(ERR_MISSING_TEMPLATE);
 }
 
-if (!values.filename) {
-  console.error("Error: Filename is required.");
-  throw new TypeError(ERR_MISSING_FILENAME);
+// ===========
+// Main
+// ===========
+const { url } = buildTemplate();
+log("Template URL", url);
+const filepath = await resolveOutput();
+log("Filepath", filepath);
+
+const response = await fetch(url);
+const responseText = await response.text();
+
+if (!response.ok) {
+  log("Response", responseText);
+  throw new TypeError(ERR_FETCH_TEMPLATE);
 }
+
+await writeFile(filepath, responseText, { encoding: "utf-8" });
+log(`Template written to ${filepath}`);
